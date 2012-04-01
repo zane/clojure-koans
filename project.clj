bdbb56273f3b615b0079adf3b75eca4f82e3f2ff
Zane Shelby <zaneshelby@gmail.com>
2012-04-01 13:06:49 -0500

    Add lein-swank as a plugin.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [koan-engine "0.1.2-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0" :exclusions [org.clojure/clojure]]
-                     [lein-koan "0.1.0"]])
+                     [lein-koan "0.1.0"]]
+  :plugins [[lein-swank "1.4.4"]])
